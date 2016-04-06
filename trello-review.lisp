1227ef280283a106e32ad84a0f0ff3436100d1c0
wott <william.ott@medicustek.com>
2016-04-06 16:56:35 +0800

    Provide report template

--- trello-review.lisp
+++ trello-review.lisp
@@ -5,9 +5,25 @@
 ;;; "trello-review" goes here. Hacks and glory await!
 
 (let (toc-entries
-      boards)
+      boards
+      (show-comments T))
   (dotimes (i 5)
     (push (list :board (format nil "board-~a" i)) toc-entries)
-    (push (list :name (format nil "board-~a" i)) boards))
-  (html-template:fill-and-print-template #p"./report.tmpl" (list :toc-entries toc-entries
-                                                                 :boards boards)))
+    (push (list :name (format nil "board-~a" i)
+                :statuses (list (list :status "Ongoing")
+                                (list :cards (loop for y from 1 to 3
+                                                   collecting (list :name (format nil "card-~a" y)
+                                                                    :desc (format nil "This is card-~a" y)
+                                                                    :status (format nil "Card status")
+                                                                    :comments (list (list :text "Hello World"
+                                                                                          :date "2016-04-06T16:43:50,123Z"
+                                                                                          :author "otti")
+                                                                                    (list :text "This is quite neat"
+                                                                                          :date "2016-04-06T16:44:12,123Z"
+                                                                                          :author "jliu"))
+                                                                    :show-comments show-comments))))) boards))
+  (with-open-file (report "./report.html" :direction :output :if-exists :supersede)
+    (html-template:fill-and-print-template #p"./report.tmpl"
+                                           (list :toc-entries (reverse toc-entries)
+                                                 :boards (reverse boards))
+                                           :stream report)))
